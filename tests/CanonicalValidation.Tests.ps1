8192a78910dedf470a025fb9c86afd43f74cda8d
SyuanTsai <carsun00@gmail.com>
2026-09-08 12:09:37 +0800

    fix-pester-marker-input-isolation

--- tests/CanonicalValidation.Tests.ps1
+++ tests/CanonicalValidation.Tests.ps1
@@ -129,6 +129,9 @@ Describe 'Canonical Standard v1 validation adapter' {
         $script:Validator | Should -Match 'repository-validation-post-pester'
         $script:Validator | Should -Match 'supervisor-owned completion result'
         $script:Validator | Should -Match 'StandardInput \$pesterResultMarker'
+        $script:Validator | Should -Match 'StandardInputIsolation'
+        $script:Validator | Should -Match '\[Codex.Validation.StandardInputIsolation\]::Hide\(\)'
+        $script:Validator | Should -Match '\[Codex.Validation.StandardInputIsolation\]::Restore\(\)'
         $script:Validator | Should -Match 'IsolateRunnerCommandFiles'
         $script:Validator | Should -Match 'TerminateProcessTree'
         $script:Validator | Should -Match 'ProtectRunnerCommandFiles'
@@ -217,7 +220,12 @@ Describe 'Canonical Standard v1 validation adapter' {
         $script:Validator | Should -Match '\$repositoryValidatorPath'
         $script:Validator | Should -Match 'pesterRunnerPath'
         $pesterInvokeIndex = $script:Validator.IndexOf('$result = Invoke-Pester')
+        $inputHideIndex = $script:Validator.IndexOf('[Codex.Validation.StandardInputIsolation]::Hide()')
+        $inputRestoreIndex = $script:Validator.IndexOf('[Codex.Validation.StandardInputIsolation]::Restore()')
         $markerReadIndex = $script:Validator.IndexOf('$resultMarker = ([Console]::In.ReadToEnd())')
+        $inputHideIndex | Should -BeLessThan $pesterInvokeIndex
+        $inputRestoreIndex | Should -BeGreaterThan $pesterInvokeIndex
+        $markerReadIndex | Should -BeGreaterThan $inputRestoreIndex
         $markerReadIndex | Should -BeGreaterThan $pesterInvokeIndex
         $script:Validator | Should -Match 'Invoke-NativeChecked -Command \$powerShellPath'
         $script:Validator | Should -Match "'-NoProfile'"
